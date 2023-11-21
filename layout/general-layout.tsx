8ee720431a90ec48288f8187cea2e4df1db3895d
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-11-21 22:43:22 +0100

    feat: create layout

---
+++ layout/general-layout.tsx
@@ -0,0 +1,29 @@
+import Header from '@/components/header/header';
+import Head from 'next/head';
+import { ReactNode } from 'react';
+
+interface GeneralLayoutProps {
+  pageTitle: string;
+  children: ReactNode;
+}
+
+const GeneralLayout = ({ pageTitle, children }: GeneralLayoutProps) => {
+  const title = `${pageTitle} | MedAidSync`;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content="Revolutionizing Medical Adherence with Digital Solutions!"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/assets/pulse.svg" />
+      </Head>
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default GeneralLayout;
