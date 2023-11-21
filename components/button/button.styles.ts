b0a61247b3f21d681edff464e186f9b1b985830f
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-11-21 22:27:30 +0100

    feat: create button component

---
+++ components/button/button.styles.ts
@@ -0,0 +1,31 @@
+import getRemValue from '@/utils/getRemValue';
+import styled from 'styled-components';
+
+export interface IButtonStyleProps {
+  primary?: boolean;
+}
+
+export const Btn = styled.button<IButtonStyleProps>`
+  display: flex;
+  gap: 1rem;
+  padding: ${getRemValue(10)} 2rem;
+  border: 1px solid rgb(var(--color-primary));
+  border-radius: ${getRemValue(20)};
+  font-weight: 700;
+
+  & > svg {
+    width: ${getRemValue(20)};
+    height: ${getRemValue(20)};
+  }
+
+  ${({ primary }) =>
+    primary &&
+    `
+      color: rgb(var(--color-white));
+      background-color: rgb(var(--color-primary));
+
+      & > svg {
+      fill: currentColor;
+   }
+   `}
+`;
