4a11fbe4529212ebfe73dd41d761a164ee5a023a
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-11-21 21:59:08 +0100

    feat(routes): create routes file

---
+++ public/lib/routes.ts
@@ -0,0 +1,15 @@
+const home = () => '/';
+const about = () => '/about';
+const contact = () => '/contact';
+const login = () => '/login';
+const signUp = () => '/sign-up';
+
+const routes = {
+  home,
+  about,
+  contact,
+  login,
+  signUp,
+};
+
+export default routes;
