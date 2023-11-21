155b674023840ba0f42557edc02a96913948bb2e
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-11-21 22:47:00 +0100

    refactor(_app): update fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import GlobalStyles from '@/styles/app.styles';
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import { Noto_Sans } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 import { ReactElement, ReactNode } from 'react';
 
-const noto_sans = Noto_Sans({
-  subsets: ['latin'],
-  weight: '400',
-});
+export const montserrat = Montserrat({ subsets: ['latin'], display: 'swap' });
+
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
@@ -23,7 +21,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     <>
       <style jsx global>{`
         html {
-          font-family: ${noto_sans.style.fontFamily};
+          font-family: ${montserrat.style.fontFamily};
         }
       `}</style>
       <GlobalStyles />
