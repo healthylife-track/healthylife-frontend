a42befbc9e5914f77b1417b8872bea2c9abab97c
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-11-21 22:49:06 +0100

    feat: create static nav data file

---
+++ public/static-data/navItems.ts
@@ -0,0 +1,16 @@
+import routes from '../../lib/routes';
+
+export const navItems = [
+  {
+    label: 'Home',
+    href: routes.home(),
+  },
+  {
+    label: 'About',
+    href: '#',
+  },
+  {
+    label: 'Contact',
+    href: '#',
+  },
+];
