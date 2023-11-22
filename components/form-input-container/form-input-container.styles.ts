4e4e80b027469558c3e1261237983f7c6284b42c
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-11-22 18:40:19 +0100

    feat(Form input): create form input component

---
+++ components/form-input-container/form-input-container.styles.ts
@@ -0,0 +1,84 @@
+import getRemValue from '@/utils/getRemValue';
+import styled from 'styled-components';
+
+export const FormInputStyles = styled.div`
+  position: relative;
+
+  & > label {
+    font-size: ${getRemValue(20)};
+    font-weight: 600;
+    display: block;
+  }
+
+  & > button {
+    font-weight: 700;
+    font-size: ${getRemValue(11)};
+    position: relative;
+    top: -10px;
+  }
+`;
+
+export const InputFormField = styled.div`
+  position: relative;
+
+  & > label {
+    font-size: ${getRemValue(15)};
+    font-weight: 600;
+    display: block;
+
+    @media screen and (min-width: 56.25em) {
+      font-size: ${getRemValue(20)};
+    }
+  }
+
+  & > button {
+    font-weight: 700;
+    font-size: ${getRemValue(11)};
+    position: relative;
+    top: -10px;
+  }
+`;
+
+export const Input = styled.input`
+  border: 1px solid rgb(var(--color-primary), 0.7);
+  outline: none;
+  margin: 1rem 0;
+  width: 100%;
+  padding: 1rem 2rem;
+  border-radius: ${getRemValue(25)};
+
+  &::placeholder {
+    font-family: inherit;
+    font-weight: 600;
+    opacity: 0.6;
+  }
+`;
+
+export const InputFlexContainer = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+`;
+
+export const TogglePasswordBtn = styled.button`
+  position: absolute;
+  width: max-content;
+  height: max-content;
+  right: 1rem;
+  bottom: 50%;
+  transform: translateY(50%);
+
+  & > svg {
+    cursor: pointer;
+    width: ${getRemValue(20)};
+    height: ${getRemValue(20)};
+    opacity: 0.4;
+  }
+`;
+
+export const InputFooterText = styled.small`
+  position: relative;
+  color: red;
+  left: 5px;
+  top: -14px;
+`;
---
+++ components/form-input-container/form-input-container.tsx
@@ -0,0 +1,72 @@
+import { FC, InputHTMLAttributes, useEffect, useState } from 'react';
+import { useController, UseControllerProps } from 'react-hook-form';
+import {
+  InputFlexContainer,
+  InputFooterText,
+  TogglePasswordBtn,
+  InputFormField,
+  Input,
+} from './form-input-container.styles';
+import SvgIcon from '../svg-icon/svg-icon';
+
+interface IFormInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  htmlFor: string;
+  label: string;
+  isPasswordType?: boolean;
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  controller: UseControllerProps<any>;
+}
+
+const FormInputContainer: FC<IFormInputProps> = ({
+  htmlFor,
+  label,
+  isPasswordType,
+  controller,
+  ...props
+}) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(controller);
+  const [showPassword, setShowPassword] = useState(false);
+
+  useEffect(() => {
+    const hidePasswordBack = setInterval(() => {
+      if (showPassword) {
+        setShowPassword(false);
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(hidePasswordBack);
+    };
+  }, [showPassword]);
+
+  return (
+    <InputFormField>
+      <label htmlFor={htmlFor}>{label}</label>
+      <>
+        {isPasswordType ? (
+          <InputFlexContainer>
+            <Input
+              {...props}
+              {...{ ...field }}
+              type={showPassword ? 'text' : 'password'}
+            />
+            <TogglePasswordBtn
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              <SvgIcon name={!showPassword ? 'eye-open' : 'eye-close'} />
+            </TogglePasswordBtn>
+          </InputFlexContainer>
+        ) : (
+          <Input {...props} {...{ ...field }} />
+        )}
+      </>
+      <>{error && <InputFooterText>{error.message}</InputFooterText>}</>
+    </InputFormField>
+  );
+};
+
+export default FormInputContainer;
